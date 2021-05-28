f83d6742c69f289976b3aa757fcef6353ea71b38
beebrsk <burutdakorn_k@cmu.ac.th>
2021-05-28 16:05:40 +0700

    commit#2

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 <script try = "text/javascript">
     alert("Welcome to Our Site");
 </script>    
+var newText = 'new Text'
+document.getElementById('Bee').innerHTML=newText
+alert("Hello! I am an alert box!!");
